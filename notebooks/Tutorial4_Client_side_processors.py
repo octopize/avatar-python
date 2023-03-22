f2238b029166890ff3baab8646213f5deaaf7d38
Olivier Regnier-Coudert <37863704+olivierabz@users.noreply.github.com>
2023-03-22 14:46:27 +0100

    fix(tutorial): remove metadata

--- notebooks/Tutorial4_Client_side_processors.py
+++ notebooks/Tutorial4_Client_side_processors.py
@@ -6,10 +6,6 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.14.5
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
 # ---
 
 # # Tutorial 4: Client-side processors
