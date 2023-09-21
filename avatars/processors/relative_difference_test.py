adf7fe3519da72eaccba8fd2af3b5f56236d1280
Morgan Guillaudeux <31599369+mguillaudeux@users.noreply.github.com>
2023-09-21 12:09:29 +0200

    fix: ban using processor with nan in references (#76)

--- avatars/processors/relative_difference_test.py
+++ avatars/processors/relative_difference_test.py
@@ -8,17 +8,17 @@ from avatars.processors import RelativeDifferenceProcessor
 ORIGINAL_DF = pd.DataFrame(
     {
         "variable_1": [100, 150, 120, 100],
-        "variable_5": [110, 180, 130, np.nan],  # to test v5 >= v1
-        "variable_6": [210, 340, 280, np.nan],  # to test v6 >= v1 + v7
-        "variable_7": [110, 180, 130, np.nan],  # to test v6 >= v1 + v7
+        "variable_5": [110, 180, 130, 100],
+        "variable_6": [210, 340, 280, 220],
+        "variable_7": [110, 180, 130, np.nan],
     }
 )
 TRANSFORMED_DF = pd.DataFrame(
     {
         "variable_1": [100, 150, 120, 100],
-        "variable_5": [10, 30, 10, np.nan],  # to test v5 >= v1
-        "variable_6": [0, 10, 30, np.nan],  # to test v6 >= v1 + v7
-        "variable_7": [110, 180, 130, np.nan],  # to test v6 >= v1 + v7
+        "variable_5": [10, 30, 10, 0],
+        "variable_6": [0, 10, 30, 20],
+        "variable_7": [110, 180, 130, np.nan],
     }
 )
 
@@ -34,6 +34,7 @@ def test_preprocess_one_variable() -> None:
     pd_testing.assert_frame_equal(
         TRANSFORMED_DF[["variable_1", "variable_5"]],
         df[["variable_1", "variable_5"]],
+        check_dtype=False,
     )
 
 
@@ -42,13 +43,14 @@ def test_preprocess_many_variables() -> None:
     # of other variables.
     processor = RelativeDifferenceProcessor(
         target="variable_6",
-        references=["variable_1", "variable_7"],
+        references=["variable_1", "variable_5"],
     )
     df = processor.preprocess(df=ORIGINAL_DF)
 
     pd_testing.assert_frame_equal(
-        TRANSFORMED_DF[["variable_1", "variable_6", "variable_7"]],
-        df[["variable_1", "variable_6", "variable_7"]],
+        TRANSFORMED_DF[["variable_6"]],
+        df[["variable_6"]],
+        check_dtype=False,
     )
 
 
@@ -70,14 +72,39 @@ def test_postprocess_many_variables() -> None:
     # Check that one variable is transformed into a relative difference to a group of
     # other variables can be re-expressed in its original form.
     processor = RelativeDifferenceProcessor(
-        target="variable_6", references=["variable_1", "variable_7"]
+        target="variable_6", references=["variable_1", "variable_5"]
     )
-    df = processor.postprocess(source=None, dest=TRANSFORMED_DF)
+    pre_df = processor.preprocess(df=ORIGINAL_DF)
+    post_df = processor.postprocess(source=None, dest=pre_df)
 
     pd_testing.assert_frame_equal(
-        ORIGINAL_DF[["variable_1", "variable_6", "variable_7"]],
-        df[["variable_1", "variable_6", "variable_7"]],
+        ORIGINAL_DF[["variable_1", "variable_5", "variable_6"]],
+        post_df[["variable_1", "variable_5", "variable_6"]],
+        check_dtype=False,
     )
+
+
+def test_preprocess_postprocess_many_variables() -> None:
+    processor = RelativeDifferenceProcessor(
+        target="variable_6", references=["variable_1", "variable_5"]
+    )
+    pre_df = processor.preprocess(df=ORIGINAL_DF)
+    post_df = processor.postprocess(source=ORIGINAL_DF, dest=pre_df)
+
+    pd_testing.assert_frame_equal(
+        ORIGINAL_DF,
+        post_df,
+        check_dtype=False,
+    )
+
+
+def test_preprocess_raises_when_nan_in_references() -> None:
+    processor = RelativeDifferenceProcessor(
+        target="variable_1",
+        references=["variable_7"],
+    )
+    with pytest.raises(ValueError, match="Expected no missing values for `references`"):
+        processor.preprocess(df=ORIGINAL_DF)
 
 
 def test_preprocess_with_rename_nodrop() -> None:
@@ -118,6 +145,7 @@ def test_postprocess_with_rename(drop_original_target: bool) -> None:
     pd_testing.assert_frame_equal(
         ORIGINAL_DF[["variable_1", "variable_5"]],
         df[["variable_1", "variable_5"]],
+        check_dtype=False,
     )
 
 
@@ -134,6 +162,7 @@ def test_preprocess_with_scaling_unit(scaling_unit: int) -> None:
     pd_testing.assert_frame_equal(
         TRANSFORMED_DF[["variable_5"]] / scaling_unit,
         df[["variable_5"]],
+        check_dtype=False,
     )
 
 
@@ -148,7 +177,7 @@ def test_postprocess_with_scaling_unit(scaling_unit: int) -> None:
     df = processor.preprocess(df=ORIGINAL_DF)
     df = processor.postprocess(source=None, dest=df)
 
-    pd_testing.assert_frame_equal(df, ORIGINAL_DF)
+    pd_testing.assert_frame_equal(df, ORIGINAL_DF, check_dtype=False)
 
 
 def test_preprocess_wrong_parameters() -> None:
