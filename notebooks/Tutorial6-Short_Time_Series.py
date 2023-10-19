39625abeedb055a76c414670567c9fa87eb167ab
Olivier Regnier-Coudert <37863704+olivierabz@users.noreply.github.com>
2023-10-19 16:51:17 +0200

    fix(notebook): rename tuto 6

--- notebooks/Tutorial6-Short_Time_Series.py
+++ notebooks/Tutorial6-Short_Time_Series.py
@@ -6,6 +6,10 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
 # ---
 
 # # Tutorial 6: Time Series
