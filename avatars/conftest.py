40e5ea962e52f624e79fc9e99209312742b70d11
Tom Crasset <25140344+tcrasset@users.noreply.github.com>
2024-02-09 16:06:22 +0100

    test: add testing infrastructure and new tests

---
+++ avatars/conftest.py
@@ -0,0 +1,25 @@
+from typing import Callable, Optional
+import httpx
+from typing import Optional
+
+from avatars.client import ApiClient
+
+RequestHandle = Callable[[httpx.Request], httpx.Response]
+
+
+def api_client_factory(handler: Optional[RequestHandle] = None) -> ApiClient:
+    """Generate an API client with a mock transport.
+
+    The handler returns an empty 200 response by default.
+    Consider overriding it with a custom handler for more complex tests.
+    """
+    if handler is None:
+        handler = lambda request: httpx.Response(200, json={})
+
+    transport = httpx.MockTransport(handler)
+    http_client = httpx.Client(base_url="http://localhost:8000", transport=transport)
+    return ApiClient(
+        base_url="http://localhost:8000",
+        http_client=http_client,
+        verify_auth=False,
+    )
